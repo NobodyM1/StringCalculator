30e75c69296b516f2dca0d87968cdbc13b8c49a9
Bit <Bit@10.20.208.121>
2016-09-16 16:18:07 +0200

    Assignment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,45 @@
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
 		
+		final Pattern pattern = Pattern.compile("-?\\d+");
+		final Matcher matcher = pattern.matcher(numbersStr);
+		
+		final ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		while(matcher.find()){
+			numbers.add(Integer.parseInt(matcher.group()));
+		}
+		
+		int i;
+		int sum = 0;
+		for(i=0; i<numbers.size(); i++){
+			if(numbers.get(i) < 0){
+				throw new StringCalculatorException();
+			}
+			if(numbers.get(i)>1000){
+				continue;
+			}
+			sum = sum + numbers.get(i);
+		}
+		
+		/*
+		String numbers = numbersStr.replaceAll("[^0-9]", "");
+		
+		int index = 0;
+		int result = 0;
+		for(int i=0; i<numbers.length(); i++){
+			String number = numbers.substring(i);
+			int num = Integer.parseInt(number);
+			result = result + num;
+		}
+		*/
+		
 		// not yet implemented
-		return 0;
+		return sum;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -1,12 +1,44 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StringCalculatorTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCorrectString() throws StringCalculatorException {
+		StringCalculator test = new StringCalculator();
+		int result = test.add("1\n2,3");
+		assertEquals(result, 6);
+	}
+	
+	@Test
+	public void test_negativeNumber() throws StringCalculatorException {
+		StringCalculator test = new StringCalculator();
+		test.add("1\n2,3..-4");
+		// Is supposed to fail
+		
+	}
+	
+	@Test
+	public void test_NumberOverThousand() throws StringCalculatorException {
+		StringCalculator test = new StringCalculator();
+		int result = test.add("1\n2,3.1001");
+		assertEquals(result, 6);
+	}
+	
+	@Test
+	public void test_AnyLenght() throws StringCalculatorException {
+		StringCalculator test = new StringCalculator();
+		int result = test.add("//[***]\n1***2***3");
+		assertEquals(result, 6);
+	}
+	
+	@Test
+	public void test_MultipleDelimeters() throws StringCalculatorException {
+		StringCalculator test = new StringCalculator();
+		int result = test.add("//[*][%]\n1*2%3");
+		assertEquals(result, 6);
 	}
 
 }
